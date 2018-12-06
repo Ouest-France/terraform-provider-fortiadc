e769edca6a52f4db2405247c1641e91b08a181b6
Pablo RUTH <contact@pablo-ruth.fr>
2018-12-06 10:31:06 +0100

    [fortiadc_loadbalance_pool_member] add read and update

--- fortiadc/resource_fortiadc_loadbalance_pool_member.go
+++ fortiadc/resource_fortiadc_loadbalance_pool_member.go
@@ -2,6 +2,7 @@ package fortiadc
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/Ouest-France/gofortiadc"
 	"github.com/hashicorp/terraform/helper/schema"
@@ -22,6 +23,7 @@ func resourceFortiadcLoadbalancePoolMember() *schema.Resource {
 			"pool": &schema.Schema{
 				Type:     schema.TypeString,
 				Required: true,
+				ForceNew: true,
 			},
 			"status": &schema.Schema{
 				Type:     schema.TypeString,
@@ -109,10 +111,95 @@ func resourceFortiadcLoadbalancePoolMemberCreate(d *schema.ResourceData, m inter
 }
 
 func resourceFortiadcLoadbalancePoolMemberRead(d *schema.ResourceData, m interface{}) error {
+	client := m.(*gofortiadc.Client)
+
+	res, err := client.LoadbalanceGetPoolMember(d.Get("pool").(string), d.Id())
+	if err != nil {
+		return err
+	}
+
+	d.Set("name", res.RealServerID)
+	d.Set("status", res.Status)
+
+	port, err := strconv.ParseInt(res.Port, 10, 64)
+	if err != nil {
+		return err
+	}
+	d.Set("port", port)
+
+	weight, err := strconv.ParseInt(res.Weight, 10, 64)
+	if err != nil {
+		return err
+	}
+	d.Set("weight", weight)
+
+	connLimit, err := strconv.ParseInt(res.Connlimit, 10, 64)
+	if err != nil {
+		return err
+	}
+	d.Set("conn_limit", connLimit)
+
+	recover, err := strconv.ParseInt(res.Recover, 10, 64)
+	if err != nil {
+		return err
+	}
+	d.Set("recover", recover)
+
+	warmup, err := strconv.ParseInt(res.Warmup, 10, 64)
+	if err != nil {
+		return err
+	}
+	d.Set("warmup", warmup)
+
+	warmrate, err := strconv.ParseInt(res.Warmrate, 10, 64)
+	if err != nil {
+		return err
+	}
+	d.Set("warmrate", warmrate)
+
+	connectionRateLimit, err := strconv.ParseInt(res.ConnectionRateLimit, 10, 64)
+	if err != nil {
+		return err
+	}
+	d.Set("conn_rate_limit", connectionRateLimit)
+
 	return nil
 }
 
 func resourceFortiadcLoadbalancePoolMemberUpdate(d *schema.ResourceData, m interface{}) error {
+	client := m.(*gofortiadc.Client)
+
+	req := gofortiadc.LoadbalancePoolMember{
+		Mkey:                     d.Id(),
+		Address:                  "0.0.0.0",
+		Address6:                 "::",
+		Backup:                   "disable",
+		ConnectionRateLimit:      fmt.Sprintf("%d", d.Get("conn_rate_limit").(int)),
+		Connlimit:                fmt.Sprintf("%d", d.Get("conn_limit").(int)),
+		Cookie:                   "",
+		HcStatus:                 "1",
+		HealthCheckInherit:       "enable",
+		MHealthCheck:             "disable",
+		MHealthCheckRelationship: "AND",
+		MHealthCheckList:         "",
+		MysqlGroupID:             "0",
+		MysqlReadOnly:            "disable",
+		Port:                     fmt.Sprintf("%d", d.Get("port").(int)),
+		RealServerID:             d.Get("name").(string),
+		Recover:                  fmt.Sprintf("%d", d.Get("recover").(int)),
+		RsProfileInherit:         "enable",
+		Ssl:                      "disable",
+		Status:                   d.Get("status").(string),
+		Weight:                   fmt.Sprintf("%d", d.Get("weight").(int)),
+		Warmup:                   fmt.Sprintf("%d", d.Get("warmup").(int)),
+		Warmrate:                 fmt.Sprintf("%d", d.Get("warmrate").(int)),
+	}
+
+	err := client.LoadbalanceUpdatePoolMember(d.Get("pool").(string), d.Id(), req)
+	if err != nil {
+		return err
+	}
+
 	return resourceFortiadcLoadbalancePoolMemberRead(d, m)
 }
 
